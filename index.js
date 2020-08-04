f66cf285a3ad15960f44f528bb04079538868171
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-08-03 22:59:42 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+});
+
+
+document.addEventListener( "DOMContentLoaded", function () {
+updateDOM()
+} );
+
+
+function updateDOM() {
+document.getElementById( "text" )
+  .innerHTML = "This is really cool!";
+}
